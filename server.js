e73b6d7498857067c35e76f8dfd5cc06a5635591
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-07-09 13:45:29 +0200

    accepting cards anpgeasst und nur 1 config

--- server.js
+++ server.js
@@ -7,60 +7,83 @@ const wss = new WebSocket.Server({ port: 8080, clientTracking: true });
 const createPlayer = require('mplayer-wrapper');
 const player = createPlayer();
 
+//Farbiges Logging
+const colors = require('colors');
+
+//Befehle auf Kommandzeile ausfuehren
+const { execSync } = require('child_process');
+
 //Lautstaerke auf 100% setzen
 let changeVolumeCommand = "sudo amixer sset PCM 100% -M";
 console.log(changeVolumeCommand)
-execSync(changeVolumeCommand);
-
-//Wenn Playlist fertig ist
-player.on('playlist-finish', () => {
-    console.log("playlist finished");
-});
+//execSync(changeVolumeCommand);
 
 //Wenn bei Track change der Filename geliefert wird
 player.on('filename', (filename) => {
-    console.log(filename);
 
+    //Ab dem Zeitpunkt wenn die Frage gestellt wird (z.B. Tiergeraeusch abgespielt), werden antworten akzeptiert
     if (filename.endsWith("-question.mp3")) {
+        console.log("start accepting cards".green);
 
         //Frage wurde gestellt, nun warten wir auf die Antwort
-        waitingForAnswer = true;
-        console.log("waiting for answer");
+        acceptingCard = true;
     }
 });
 
 //Wenn sich ein Titel aendert (durch Nutzer oder durch den Player)
 player.on('track-change', () => {
-    console.log("track-change");
 
     //Neuen Dateinamen liefern
     player.getProps(['filename']);
 });
 
-//Befehle auf Kommandzeile ausfuehren
-const { execSync } = require('child_process');
-
-//Game-Config-JSON-Objekt aus Datei holen
-console.log("read game config");
+//Game-Config-JSON-Objekt aus Datei holen, um daraus passende Datenstruktur zu bauen
 const fs = require('fs-extra');
-const gameConfig = fs.readJsonSync('./game-config.json');
+console.log("read game config".green);
+const gameConfigJSON = fs.readJsonSync('../SoundQuizGPIO/config.json');
 
-//Verzeichnis wo die Videos liegen
-const soundDir = "/media/soundquiz";
-//const soundDir = "C:/Apache24/htdocs/SoundQuizServer/sounds";
+//Datenstruktur fuer Server (zufaellige Fragen laden)
+var gameConfig = {};
+
+//Ueber Karten gehen
+for (let card in gameConfigJSON) {
+
+    //Karteninfo laden
+    let cardInfo = gameConfigJSON[card];
+
+    //Wenn es eine Antwortkarte ist
+    if (cardInfo["type"] === "answer") {
+
+        //Ueber die Spiele gehen, denen diese Karte zugeordnet ist
+        for (let game of cardInfo["games"]) {
+
+            //Wenn es bei diesem Spiel noch keine Eintraege gibt ein Array anlagen, ansonsten weiteren Wert hinzufuegen
+            !gameConfig[game] ? gameConfig[game] = [cardInfo["value"]] : gameConfig[game].push(cardInfo["value"])
+        }
+    }
+}
+console.log("available games and questions: " + JSON.stringify(gameConfig).green);
+
+//Wo wird das Skript betrieben win vs. pi
+const runMode = process.argv[2] ? process.argv[2] : "pi";
+
+//Verzeichnis wo die Fragenfiles liegen
+const soundDir = runMode === "win" ? "C:/Apache24/htdocs/SoundQuizServer/sounds" : "/media/soundquiz";
+console.log("using sound dir " + soundDir.green);
 
 //Zu Beginn ist kein Spiel ausgewaehlt
 var currentGame = null;
 
 //Liste der Fragen und aktuelle Frage
 var currentQuestions = [];
-var currentQuestion = "";
+var currentQuestion = null;
 
-//Werden gerade Antworten ausgewertet?
-var waitingForAnswer = false;
+//Werden gerade Karten ausgewertet?
+var acceptingCard = false;
 
 //"Welches Spiel moechtest du spielen?"
 playSound("which-game");
+console.log("select game".green);
 
 //Wenn sich ein WebSocket mit dem WebSocketServer verbindet
 wss.on('connection', function connection(ws) {
@@ -85,62 +108,62 @@ wss.on('connection', function connection(ws) {
 
             //Infos kommen per RFID-Karten
             case "send-card-data":
-                console.log(value);
 
-                //Kartenwerte auslesen
-                let cardData = JSON.parse(value);
-                let cardDataType = cardData.type;
-                let cardDataValue = cardData.value;
+                //Wenn gerade Karten akzeptiert werden oder noch kein Spiel geladen wurde
+                if (acceptingCard || currentGame === null) {
+                    console.log(value);
 
-                //Welcher Typ von Karte ist es (game-select vs. answer)
-                switch (cardDataType) {
+                    //Kartenwerte auslesen
+                    let cardData = JSON.parse(value);
+                    let cardDataType = cardData.type;
+                    let cardDataValue = cardData.value;
 
-                    //Spiel beenden
-                    case "shutdown":
-                        console.debug("stop game and shutdown");
+                    //Welcher Typ von Karte ist es (game-select vs. answer)
+                    switch (cardDataType) {
 
-                        //Verabschiedungssound
-                        playSound("shutdown", true);
+                        //Spiel beenden
+                        case "shutdown":
+                            console.log("user send stop game and shutdown event".green);
+                            console.log("stop accepting cards".red);
+                            acceptingCard = false;
 
-                        //Pi herunterfahren
-                        //execSync("shutdown -h now");
-                        break;
+                            //Verabschiedungssound
+                            playSound("shutdown", true);
 
-                    //Spielauswahl
-                    case "game-select":
-                        console.log("user sends game select event");
+                            //Pi herunterfahren
+                            //execSync("shutdown -h now");
+                            break;
 
-                        //keine Antworten akzeptieren
-                        waitingForAnswer = false;
+                        //Spielauswahl
+                        case "game-select":
+                            console.log("user sends game select event".green);
 
-                        //Spiel starten
-                        startGame(cardDataValue);
-                        break;
+                            //Spiel starten
+                            startGame(cardDataValue);
+                            break;
 
-                    //Bei einer Antwort
-                    case "answer":
-                        console.log("user sends answer event");
+                        //Bei einer Antwort
+                        case "answer":
+                            console.log("user sends answer event".green);
 
-                        //Wenn noch kein Spiel ausgewaehlt wurde
-                        if (currentGame === null) {
-                            console.log("waiting for game select");
+                            //Wenn noch kein Spiel ausgewaehlt wurde
+                            if (currentGame === null) {
+                                console.log("waiting for game select".yellow);
 
-                            //Aufforderung ein Spiel auszuwaehlen
-                            playSound("select-game-first", true);
-                        }
+                                //Aufforderung ein Spiel auszuwaehlen
+                                playSound("select-game-first", true);
+                            }
 
-                        //Es laeuft schon ein Spiel
-                        else {
-
-                            //Wenn gerade Antworten ausgewertet werden
-                            if (waitingForAnswer) {
+                            //Es laeuft schon ein Spiel
+                            else {
 
                                 //Wenn die Antwort korrekt ist
                                 if (cardDataValue === currentQuestion) {
-                                    console.log("correct answer. Stop waiting for answer");
+                                    console.log("correct answer".green);
 
                                     //Weitere Kartenaufrufe verhindern
-                                    waitingForAnswer = false;
+                                    console.log("stop accepting cards".red);
+                                    acceptingCard = false;
 
                                     //Info an WS-Clients, dass Antwort korrekt war
                                     messageObjArr.push({
@@ -163,7 +186,11 @@ wss.on('connection', function connection(ws) {
 
                                 //Antwort war falsch
                                 else {
-                                    console.log("wrong answer");
+                                    console.log("wrong answer".red);
+
+                                    //Weitere Kartenaufrufe verhindern
+                                    console.log("stop accepting cards".red);
+                                    acceptingCard = false;
 
                                     //Info an WS-Clients, dass Antwort falsch war
                                     messageObjArr.push({
@@ -177,16 +204,16 @@ wss.on('connection', function connection(ws) {
                                     //"Leider falsch, probier es noch einmal"
                                     playSound("answer-wrong");
 
-                                    //Frage wiederholen?
+                                    //gleiche Frage noch einmal abspielen
+                                    askQuestion(true);
                                 }
                             }
-
-                            //Antworten noch nicht freigeschaltet
-                            else {
-                                console.log("please wait");
-                            }
-                        }
-                        break;
+                    }
+                    break;
+                }
+                //Antworten noch nicht freigeschaltet
+                else {
+                    console.log("not accepting card yet".red);
                 }
                 break;
         }
@@ -213,7 +240,6 @@ function sendClientInfo(messageObjArr) {
 
     //Ueber Liste der MessageObjekte gehen
     messageObjArr.forEach(messageObj => {
-        //console.log(messageObj)
 
         //Ueber Liste der WS gehen und Nachricht schicken
         for (ws of wss.clients) {
@@ -244,17 +270,21 @@ function playSound(path, interrupt = false) {
 
 //Spiel starten
 function startGame(game) {
-    console.log("try to start game " + game);
+    console.log("try to start game " + game.green);
 
     //Wenn das bereits gestartete Spiel gestartet werden soll, Hinweis ausgeben
     if (currentGame === game) {
-        console.log("already playing game " + game);
+        console.log("already playing game " + game.yellow);
         playSound("already-playing-game", true);
     }
 
     //dieses Spiel starten
     else {
-        console.log("play game " + game);
+        console.log("play game " + game.green);
+
+        //Karten erst wieder akzeptieren nachdem Frage gestellt wurde
+        console.log("stop accepting cards".red);
+        acceptingCard = false;
 
         //Allgemein: "Los geht's. Wir spielen jetzt das Spiel..."
         playSound("lets-go", true);
@@ -267,6 +297,7 @@ function startGame(game) {
 
         //Fragen dieses Spiels aus Config laden
         currentQuestions = gameConfig[game];
+        console.log("available questions in game " + currentGame + " " + JSON.stringify(currentQuestions).yellow);
 
         //Frage stellen
         askQuestion();
@@ -274,25 +305,33 @@ function startGame(game) {
 }
 
 //Eine Frage stellen
-function askQuestion() {
-    console.log("pick next random question");
+function askQuestion(repeat = false) {
 
-    //neue Frage erstellen
-    let question;
+    if (!repeat) {
+        console.log("pick next random question".yellow);
 
-    //Zufaelligen Wert ausaehlen und sicherstellen, dass nicht 2 Mal die gleiche Frage kommt
-    do {
-        question = currentQuestions[Math.floor(Math.random() * currentQuestions.length)];
+        //neue Frage erstellen
+        let question;
+
+        //Zufaelligen Wert ausaehlen und sicherstellen, dass nicht 2 Mal die gleiche Frage kommt
+        do {
+            question = currentQuestions[Math.floor(Math.random() * currentQuestions.length)];
+        }
+        while (question === currentQuestion)
+
+        //neue Frage merken
+        currentQuestion = question;
+
+        //"Wer macht dieses Geraeusch", "Zeige mir den Buchstaben", "Wer spricht so?"
+        playSound("question-prefix-" + currentGame);
     }
-    while (question === currentQuestion)
 
-    //neue Frage merken
-    currentQuestion = question;
-    console.log("next question is " + currentQuestion);
-
-    //"Wer macht dieses Geraeusch", "Zeige mir den Buchstaben", "Wer spricht so?"
-    playSound("question-prefix-" + currentGame);
+    //gleiche Frage wird wiederholt
+    else {
+        console.log("repeat question".yellow)
+    }
 
     //Eigentlicher Sound (Katze, Mensch, Buchstabe)
+    console.log("next question is " + currentQuestion.green);
     playSound(currentGame + "/" + currentQuestion + "-question");
 }
