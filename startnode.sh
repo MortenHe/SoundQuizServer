d7b29b2e803bc0fb297eb640c3d6ab0bda6a5e7e
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-10-27 10:25:59 +0100

    startnodesound.sh

--- startnode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/pi/mh_prog/SoundQuizServer
-/usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-soundquiz-server.txt &
-/bin/sleep 5
-cd /home/pi/mh_prog/SoundQuizGPIO
-/usr/bin/sudo /usr/bin/node ./led.js > /home/pi/mh_prog/output-soundquiz-led.txt &
-/usr/bin/sudo /usr/bin/node ./rfid.js > /home/pi/mh_prog/output-soundquiz-rfid.txt &
---
+++ startnodesound.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/sudo /usr/bin/node /home/pi/mh_prog/SoundQuizServer/server.js > /home/pi/mh_prog/output-server.txt &
